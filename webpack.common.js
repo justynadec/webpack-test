ba99f7485c3136123bb55bbc3058e3ff1e64186e
Justyna Dec <justynadec@o2.pl>
2020-06-21 15:13:51 +0200

    attempt to add a new page

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/justyna.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "justyna.html",
+    }),
   ],
 };
